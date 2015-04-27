2455716f670ef38e410559c11d74f84abf7fa22d
Ali Smith <alipsmith@me.com>
2015-04-27 07:24:58 -0700

    initial commit v2

--- js/app.js
+++ js/app.js
@@ -1,33 +1,32 @@
 $(document).ready(function() {
 
-$("html,body").animate({scrollTop: 0}, 500);
+// $("html,body").animate({scrollTop: 0}, 500);
 	
-$('.arrow-down').click(function() {
-	$('html, body').animate({
-		scrollTop: $('.deliverablesH1').offset().top-100
-		}, 1500);
-});
 
 $('a').click(function() {
 	$('html, body').animate({
-		scrollTop: $( $.attr(this, 'href') ).offset().top-100
+		scrollTop: $( $.attr(this, 'href') ).offset().top
 		}, 1000);
 		return false;
 });
 
-$('.circle-box').hover(function() {
-	$(this).children('.circle-front').hide();
-	$(this).children('.circle-back').show();
-}, function() {
-	$(this).children('.circle-front').show();
-	$(this).children('.circle-back').hide();
-});
 
+// $('#samplesID').click(function() {
+// 	$('.samplesSection').show();
+// 	$('.homeSection').hide();
+// 	$('.infoSection').hide();
+// });
+
+// $('#infoID').click(function() {
+// 	$('.samplesSection').hide();
+// 	$('.homeSection').show();
+// 	$('.infoSection').show();
+// });
 
 $('.sample-1, .sample-2, .sample-3, .sample-4').hover(function() {
-	$(this).children('.samples-links').fadeIn();
+	$(this).children('.sampleOverlay').fadeIn();
 }, function() {
-	$(this).children('.samples-links').fadeOut();
+	$(this).children('.sampleOverlay').fadeOut();
 });
 
 
