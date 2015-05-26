0980357f4b13c788ebdb20e8c9e990b1734bb5e5
Ali Smith <alipsmith@me.com>
2015-05-25 17:13:17 -0700

    media query done enough

--- js/videoArrays.js
+++
@@ -1,7 +0,0 @@
-// HSUS VIDEOS
-
-var hsusOne = {'<iframe width="500" height="375" src="https://www.youtube.com/embed/7pLlt6y3ZYw?showinfo=0" frameborder="0" allowfullscreen></iframe>'};
-
-var hsusTwo = {'<iframe width="500" height="375" src="https://www.youtube.com/embed/aCPYwyPk5s4?showinfo=0" frameborder="0" allowfullscreen></iframe>'};
-
-var hsusThree = {'<iframe width="500" height="375" src="https://www.youtube.com/embed/kajBVVsJMz0?showinfo=0" frameborder="0" allowfullscreen></iframe>'}
